b1253cf00acc2b3c01a651a450f1d8d234736689
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-05-02 12:33:10 +0200

    edit function to generate draws df to add reference time and make plot in vignette

--- R/get_nowcast_pred_draw.R
+++ R/get_nowcast_pred_draw.R
@@ -1,3 +1,32 @@
+#' Get a draw of only the predicted elements of the nowcast vector
+#'
+#' @param point_nowcast_pred_matrix Matrix containing only the elements that
+#'    have not yet been observed as of the final reference date
+#' @param disp Vector of dispersion parameters indexed by horizon from minus
+#'    one to the maximum delay
+#'
+#' @returns Vector of predicted draws at each reference time
+#' @export
+#'
+#' @examples
+#' point_nowcast_pred_matrix <-
+#'   matrix(
+#'     c(
+#'       NA, NA, NA, NA,
+#'       NA, NA, NA, NA,
+#'       NA, NA, NA, 16.8,
+#'       NA, NA, 21.2, 19.5,
+#'       NA, 34.5, 15.4, 9.1
+#'     ),
+#'     nrow = 4,
+#'     byrow = TRUE
+#'   )
+#' disp <- c(0.8, 12.4, 9.1)
+#' nowcast_pred_draw <- get_nowcast_pred_draw(
+#'   point_nowcast_pred_matrix,
+#'   disp
+#' )
+#' nowcast_pred_draw
 get_nowcast_pred_draw <- function(point_nowcast_pred_matrix,
                                   disp) {
   n_horizons <- length(disp)
@@ -11,6 +40,41 @@ get_nowcast_pred_draw <- function(point_nowcast_pred_matrix,
   return(draw_pred)
 }
 
+#' Get a dataframe of multiple draws of only the predicted elements of the
+#'    nowcast vector
+#'
+#' @param point_nowcast_pred_matrix Matrix containing only the elements that
+#'    have not yet been observed as of the final reference date
+#' @param disp Vector of dispersion parameters indexed by horizon from minus
+#'    one to the maximum delay
+#' @param n_draws Integer indicating the number of draws of the predicted
+#'    nowcast vector to generate. Default is `1000`.
+#'
+#' @returns Dataframe containing the predicted point nowcast vectors indexed by
+#'    reference time (`pred_count`), reference time (`time`), and the draw index
+#'    (`draw`).
+#' @export
+#'
+#' @examples
+#' point_nowcast_pred_matrix <-
+#'   matrix(
+#'     c(
+#'       NA, NA, NA, NA,
+#'       NA, NA, NA, NA,
+#'       NA, NA, NA, 16.8,
+#'       NA, NA, 21.2, 19.5,
+#'       NA, 34.5, 15.4, 9.1
+#'     ),
+#'     nrow = 4,
+#'     byrow = TRUE
+#'   )
+#' disp <- c(0.8, 12.4, 9.1)
+#' nowcast_pred_draws_df <- get_nowcast_pred_draw(
+#'   point_nowcast_pred_matrix,
+#'   disp,
+#'   500
+#' )
+#' nowcast_pred_draws_df
 get_nowcast_pred_draws <- function(point_nowcast_pred_matrix,
                                    disp,
                                    n_draws = 1000) {
@@ -23,6 +87,7 @@ get_nowcast_pred_draws <- function(point_nowcast_pred_matrix,
     )
     df_i <- data.frame(
       pred_count = pred_nowcast_vec,
+      time = 1:length(pred_nowcast_vec),
       draw = i
     )
     if (i == 1) {

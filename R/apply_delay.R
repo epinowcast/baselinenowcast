9c8438b7854fae9a52e05e3aed9b9370b8997c17
Sam Abbott <contact@samabbott.co.uk>
2025-05-19 17:08:16 +0100

    fix: simplify .calc_expectation to return early if no NAs in column (inspired by KJ's suggestion in #145)

--- R/apply_delay.R
+++ R/apply_delay.R
@@ -90,27 +90,21 @@ apply_delay <- function(rep_tri_to_nowcast, delay_pmf) {
     delay_cdf,
     n_rows) {
   # Find rows with NA in this column that need to be filled
-  col_index <- index
-  na_rows <- .where_is_na_in_col(expectation, col_index)
+  na_rows <- .where_is_na_in_col(expectation, index)
 
-  while (length(na_rows) == 0) {
-    col_index <- col_index + 1
-    # Stop if we've reached the end of the matrix
-    if (col_index > ncol(expectation)) {
-      return(expectation)
-    }
-    na_rows <- .where_is_na_in_col(expectation, col_index)
+  if (length(na_rows) == 0) {
+    return(expectation)
   }
 
   # Start with the first row that has NA
   row_start <- min(na_rows)
-  delay_prob <- delay_pmf[col_index]
-  delay_cdf_prev <- delay_cdf[col_index - 1]
+  delay_prob <- delay_pmf[index]
+  delay_cdf_prev <- delay_cdf[index - 1]
 
   # Extract the left block for these rows
   block_bottom_left <- .extract_block_bottom_left(
     expectation,
-    col_index,
+    index,
     n_rows,
     row_start
   )
@@ -122,7 +116,7 @@ apply_delay <- function(rep_tri_to_nowcast, delay_pmf) {
   exp_N <- .calc_modified_expectation(x, delay_cdf_prev)
 
   # Update only the NA rows in the column
-  expectation[row_start:n_rows, col_index] <- exp_N * delay_prob
+  expectation[row_start:n_rows, index] <- exp_N * delay_prob
 
   return(expectation)
 }

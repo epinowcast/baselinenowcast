2e397fb4ef499c892d9bde39ed231f982233b938
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-08-04 09:39:32 +0100

    add covid data to package for testing

---
+++ data-raw/create_covid_data.R
@@ -0,0 +1,32 @@
+# Script to generate large covid dataset for baselinenowcast tests.
+# Not currently used as package data.
+# Run this script when the data needs to be updated
+library(readr)
+covid_url <- "https://raw.githubusercontent.com/KITmetricslab/hospitalization-nowcast-hub/11c745322c055cfbd4f0c8f72241642a50aea399/data-truth/COVID-19/COVID-19_hospitalizations_preprocessed.csv"
+raw_data <- read_csv(covid_url) |>
+  rename(value_81d = `value_>80d`)
+
+raw_data_long <- pivot_longer(
+  raw_data,
+  cols = starts_with("value_"),
+  names_to = "delay",
+  values_to = "count",
+  names_prefix = "value_"
+) |>
+  mutate(
+    delay = as.integer(gsub("d.*$", "", delay)),
+    report_date = date + days(delay)
+  ) |>
+  rename(reference_date = date) |>
+  filter(
+    location == "DE",
+    delay <= 40
+  )
+
+# Save the data to fixtures folder in testthat
+saveRDS(raw_data_long, file.path(
+  "tests",
+  "testthat",
+  "fixtures",
+  "covid_data.rds"
+))

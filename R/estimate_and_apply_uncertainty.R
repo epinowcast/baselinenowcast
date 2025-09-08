422d440aff97ddab42c9588110b55841d281f8c5
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-09-08 11:00:58 +0100

    update docs and add the additional args

--- R/estimate_and_apply_uncertainty.R
+++ R/estimate_and_apply_uncertainty.R
@@ -39,33 +39,47 @@
 #'   n_retrospective_nowcasts = 1
 #' )
 #' nowcast_draws_df
-estimate_and_apply_uncertainty <- function(point_nowcast_matrix,
-                                           reporting_triangle,
-                                           max_delay,
-                                           n_retrospective_nowcasts =
-                                             round(1.5 * max_delay),
-                                           draws = 100,
-                                           error_model = "negative binomial",
-                                           aggregator = sum,
-                                           ...) {
+estimate_and_apply_uncertainty <- function(
+    point_nowcast_matrix,
+    reporting_triangle,
+    max_delay,
+    n_history_delay = round(1.5 * max_delay),
+    n_retrospective_nowcasts = 3 * max_delay - n_history_delay,
+    draws = 100,
+    uncertainty_model = fit_by_horizon,
+    uncertainty_sampler = sample_nb,
+    ref_time_aggregator = identity,
+    delay_aggregator = function(x) rowSums(x, na.rm = TRUE),
+    ...) {
+  # Estimate uncertainty from the reporting triangle passed in.
   trunc_rep_tris <- truncate_triangles(reporting_triangle,
     n = n_retrospective_nowcasts
   )
 
   retro_rep_tris <- construct_triangles(trunc_rep_tris)
 
-  retro_pt_nowcasts <- fill_triangles(retro_rep_tris)
+  retro_pt_nowcasts <- fill_triangles(retro_rep_tris,
+    max_delay = max_delay,
+    n = n_history_delay
+  )
 
-  disp <- estimate_uncertainty(
-    retro_pt_nowcasts,
-    trunc_rep_tris,
-    retro_rep_tris
+  uncertainty_params <- estimate_uncertainty(
+    point_nowcast_matrices = retro_pt_nowcasts,
+    truncated_reporting_triangles = trunc_rep_tris,
+    retro_reporting_triangles = retro_rep_tris,
+    n = n_retrospective_nowcasts,
+    uncertainty_model = uncertainty_model,
+    ref_time_aggregator = ref_time_aggregator,
+    delay_aggregator = delay_aggregator
   )
   nowcast_draws <- sample_nowcasts(
     point_nowcast_matrix,
     reporting_triangle,
-    disp,
-    draws
+    uncertainty_params = uncertainty_params,
+    draws = draws,
+    uncertainty_sample = uncertainty_sampler,
+    ref_time_aggregator = ref_time_aggregator,
+    delay_aggregator = delay_aggregator
   )
   return(nowcast_draws)
 }

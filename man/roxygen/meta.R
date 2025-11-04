32df464e0c23d83f6ed7fa5cd62a6492b3afa208
Sam Abbott <contact@samabbott.co.uk>
2025-11-04 18:17:22 +0000

    docs: add roxygen meta.R to define family titles
    Add man/roxygen/meta.R to centralise family title definitions for:
    - estimate_delay
    - generate_retrospective_data
    - generate_point_nowcasts
    - estimate_observation_error
    - generate_probabilistic_nowcasts
    
    This improves documentation readability by showing descriptive titles
    instead of raw family tag names in 'See Also' sections

---
+++ man/roxygen/meta.R
@@ -0,0 +1,9 @@
+list(
+  rd_family_title = list(
+    estimate_delay = "Delay distribution estimation functions",
+    generate_retrospective_data = "Retrospective data generation functions",
+    generate_point_nowcasts = "Point nowcast generation functions",
+    estimate_observation_error = "Observation error estimation functions",
+    generate_probabilistic_nowcasts = "Probabilistic nowcast generation functions"
+  )
+)

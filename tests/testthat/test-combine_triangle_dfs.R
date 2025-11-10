8b7c1d4dd54e78ec713c225ff02ba96bcc84b5e5
Sam Abbott <contact@samabbott.co.uk>
2025-11-10 14:18:06 +0000

    Fix line length in test-combine_triangle_dfs.R

--- tests/testthat/test-combine_triangle_dfs.R
+++ tests/testthat/test-combine_triangle_dfs.R
@@ -31,8 +31,12 @@ example_data <- data.frame(
 
 expected_cols <- c("reference_date", "report_date", "count")
 strata_cols <- c("location", "age_group")
-covid_data_subset <- covid_data[covid_data$age_group %in% c("00+", "60-79", "80+"), ]
-covid_data_single_strata <- covid_data_subset[covid_data_subset$age_group == "00+", ]
+covid_data_subset <- covid_data[
+  covid_data$age_group %in% c("00+", "60-79", "80+"),
+]
+covid_data_single_strata <- covid_data_subset[
+  covid_data_subset$age_group == "00+",
+]
 
 test_that(".combine_triangle_dfs combines data across strata correctly", {
   result <- .combine_triangle_dfs(

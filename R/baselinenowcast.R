6b69c6ad076adc952d902c8710f23f06ff3ffece
Sam Abbott <contact@samabbott.co.uk>
2025-11-12 18:31:32 +0000

    Fix line length in baselinenowcast examples
    Split long comment lines to comply with 80 character limit.

--- R/baselinenowcast.R
+++ R/baselinenowcast.R
@@ -85,7 +85,8 @@ baselinenowcast <- function(data,
 #' @method baselinenowcast reporting_triangle
 #' @returns Data.frame of class \code{\link{baselinenowcast_df}}
 #' @examples
-#' # Filter to recent data and truncate to reasonable max_delay for faster example
+#' # Filter to recent data and truncate to reasonable max_delay for faster
+#' # example
 #' data_as_of_df <- syn_nssp_df[syn_nssp_df$report_date <= "2026-04-01", ]
 #' rep_tri <- as_reporting_triangle(data = data_as_of_df) |>
 #'   truncate_to_delay(max_delay = 25) |>
@@ -245,7 +246,8 @@ baselinenowcast.reporting_triangle <- function(
 #' @method baselinenowcast data.frame
 #' @returns Data.frame of class \code{\link{baselinenowcast_df}}
 #' @examples
-#' # Filter data to exclude most recent report dates and limit to 75 reference dates
+#' # Filter data to exclude most recent report dates and limit to 75
+#' # reference dates
 #' max_ref_date <- max(germany_covid19_hosp$reference_date)
 #' min_ref_date <- max_ref_date - 74
 #' covid_data_to_nowcast <- germany_covid19_hosp[

a52240802f7e58925473e3db460d02e95989735a
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-12-18 09:51:38 -0500

    fix urls and description

--- R/data.R
+++ R/data.R
@@ -158,7 +158,7 @@
 #'    YYYY-MM-DD.}
 #' }
 #' @source This data comes directly from the preprocessed data in the
-#'   [German COVID-19 Nowcast Hub](https://github.com/KITmetricslab/hospitalization-nowcast-hub/tree/main/data-truth/COVID-19/COVID-19_hospitalizations_preprocessed.csv). #nolint
+#'   [German COVID-19 Nowcast Hub](https://github.com/KITmetricslab/hospitalization-nowcast-hub/blob/main/data-truth/COVID-19/COVID-19_hospitalizations_preprocessed.csv). #nolint
 #'   It contains incident case counts by age group in Germany.
 #' @family example_data
 "germany_covid19_hosp"

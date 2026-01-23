15121afff37b61190239686f46d60de0270df81f
Kaitlyn Johnson <94390107+kaitejohnson@users.noreply.github.com>
2026-01-23 12:03:24 +0000

    Issue 262: Prepare for Cran submission (#357)
    * update aspirational installation instructions, review description
    
    * update description
    
    * Add GitHub links to DESCRIPTION
    
    * fix urls and description
    
    * update changelog with description of key features of the package
    
    * update wordlist
    
    * update version
    
    * fixes per coderabbit suggestions
    
    * Update vignettes/chunks/_readme-install-baselinenowcast.Rmd
    
    Co-authored-by: Sam Abbott <contact@samabbott.co.uk>
    
    * Update vignettes/chunks/_readme-install-baselinenowcast.Rmd
    
    Co-authored-by: Sam Abbott <contact@samabbott.co.uk>
    
    * remove verbose changes from the changelog
    
    * only run checks on merges to main
    
    * try new url handling in docs
    
    * remove cran_comments
    
    * add url to all urls in docs
    
    * fix description
    
    * fix url
    
    * add sentence to describe the release
    
    * add lifecycle stable
    
    * fix empty line
    
    * update wordlist, add nolint, add epinowcast universe badge
    
    * review edits
    
    * Update R-CMD-check.yaml
    
    * Update DESCRIPTION
    
    Co-authored-by: Sam Abbott <contact@samabbott.co.uk>
    
    * Update DESCRIPTION
    
    Co-authored-by: Sam Abbott <contact@samabbott.co.uk>
    
    * fix formatting
    
    * Add entries to .Rbuildignore for new files
    
    * Add profile_original.html to .Rbuildignore
    
    * remove .htmls
    
    * add rhub
    
    * fix html
    
    * reduce figure dpi
    
    ---------
    
    Co-authored-by: Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
    Co-authored-by: Sam Abbott <contact@samabbott.co.uk>

--- R/estimate_uncertainty.R
+++ R/estimate_uncertainty.R
@@ -409,7 +409,7 @@ fit_by_horizon <- function(obs,
 #'   binomial. This code was adapted from code written (under an MIT license)
 #'   by the Karlsruhe Institute of Technology RESPINOW German Hospitalization
 #'   Nowcasting Hub.
-#'   Modified from: https://github.com/KITmetricslab/RESPINOW-Hub/blob/7fab4dce7b559c3076ab643cf22048cb5fb84cc2/code/baseline/functions.R#L404 #nolint
+#'   Modified from: \url{https://github.com/KITmetricslab/RESPINOW-Hub/blob/7fab4dce7b559c3076ab643cf22048cb5fb84cc2/code/baseline/functions.R#L404} #nolint
 #' @importFrom stats dnbinom optimize
 #' @param x Vector of observed values.
 #' @param mu Vector of expected values.
--- R/preprocess.R
+++ R/preprocess.R
@@ -14,7 +14,7 @@
 #' This code was adapted from code written (under an MIT license)
 #' by the Karlsruhe Institute of Technology RESPINOW
 #' German Hospitalization Nowcasting Hub.
-#' Modified from https://github.com/KITmetricslab/RESPINOW-Hub/blob/main/code/baseline/functions.R #nolint
+#' Modified from \url{https://github.com/KITmetricslab/RESPINOW-Hub/blob/main/code/baseline/functions.R} #nolint
 #'
 #' @param reporting_triangle A [reporting_triangle] object.
 #' @inheritParams assert_reporting_triangle
